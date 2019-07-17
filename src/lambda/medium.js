b365bc7b241439cef290c5e0afd9eb2170ea0d1c
Jens Neuhaus <kontakt@jensneuhaus.de>
2019-07-17 17:22:32 +0200

    Update medium.js

--- src/lambda/medium.js
+++ src/lambda/medium.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch"
 import xml2js from 'xml2js-es6-promise'
 
 exports.handler = async (event, context, callback) => {
-    const mediumFeed = `https://medium.com/feed/@dinosaurlord`
+    const mediumFeed = `https://medium.com/feed/@jensneuhaus`
     const response = await fetch(mediumFeed)
     const xml = await response.text()
     const data = await xml2js(xml)
